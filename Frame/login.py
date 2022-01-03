2087915cb130eb49bdd4b5ea81babfe956a5b000
errol <errolken2008@gmail.com>
2022-01-03 22:20:40 +0530

    Enter

--- Frame/login.py
+++ Frame/login.py
@@ -18,16 +18,39 @@ class Main_window:
             loginbtn.config(background='#000000',foreground='#FFFFFF',activebackground="white")
             regframe=Frame(window,width=1000,height=558,bg='#000000')
             regframe.place(x=0,y=42)
-            Label(regframe,text='Register',font=("Poppins",24),background='#000000',foreground='#FFFFFF').place(x=430,y=0)
+            Label(regframe,text='Register',font=("Poppins",24),background='#000000',foreground='#FFFFFF').place(x=430,y=50)
+            Label(regframe,text='Enter Email',font=("Poppins",20),background='#000000',foreground='#8AF8FF').place(x=325,y=140)
+            Label(regframe,text='Set Password',font=("Poppins",20),background='#000000',foreground='#8AF8FF').place(x=325,y=270)
+
+            emailentry = Entry(regframe,bd = 0,bg = "#FFFFFF",font=("Poppins",15),highlightthickness = 0)
+            emailentry.place(x=330,y=200,width=350)
             
-        
+            passentry = Entry(regframe,bd = 0,bg = "#FFFFFF",font=("Poppins",15),highlightthickness = 0)
+            passentry.place(x=330,y=330,width=350)
+
+            #submit button for register
+            reg_submit = Button(regframe,text="Submit",borderwidth = 0,highlightthickness = 0,font=("Poppins",15),command = None,background="#FFFFFF",foreground="#000000",activebackground="#FFFFFF",relief = "flat")
+            reg_submit.place(x=450,y=450,width=100,height=50)
+
         #function of login button    
         def loginrun():
             loginbtn.config(background='#FFFFFF',foreground='#000000')
             regbtn.config(background='#000000',foreground='#FFFFFF')
             loginframe=Frame(window,width=1000,height=558,bg='#000000')
             loginframe.place(x=0,y=42)
-            Label(loginframe,text='Login',font=("Poppins",24),background='#000000',foreground='#FFFFFF').place(x=440,y=0)
+            Label(loginframe,text='Login',font=("Poppins",24),background='#000000',foreground='#FFFFFF').place(x=440,y=50)
+            Label(loginframe,text='Register',font=("Poppins",24),background='#000000',foreground='#FFFFFF').place(x=430,y=50)
+            Label(loginframe,text='Email',font=("Poppins",20),background='#000000',foreground='#8AF8FF').place(x=325,y=140)
+            Label(loginframe,text='Password',font=("Poppins",20),background='#000000',foreground='#8AF8FF').place(x=325,y=270)
+
+            emailentry = Entry(loginframe,bd = 0,bg = "#FFFFFF",font=("Poppins",15),highlightthickness = 0)
+            emailentry.place(x=330,y=200,width=350)
+            
+            passentry = Entry(loginframe,bd = 0,bg = "#FFFFFF",font=("Poppins",15),highlightthickness = 0)
+            passentry.place(x=330,y=330,width=350)
+
+            login_submit = Button(loginframe,text="Submit",borderwidth = 0,highlightthickness = 0,font=("Poppins",15),command = None,background="#FFFFFF",foreground="#000000",activebackground="#FFFFFF",relief = "flat")
+            login_submit.place(x=450,y=450,width=100,height=50)
 
 
         #two labels login and register
@@ -38,7 +61,6 @@ class Main_window:
         
         #intializing a frame in root window for register
         regrun()
-
 
         window.resizable(False,False)
         window.mainloop()
